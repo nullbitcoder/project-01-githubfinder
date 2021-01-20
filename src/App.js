da8a646129074b1af80325fbeb581c8da1918805
nullbitcoder <lxxxl1146@gmail.com>
2021-01-20 23:35:26 +0100

    adding css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <h1>Hello Jesus!</h1>
+            <h1>Hello From React!</h1>
         </div>
     );
 }
