231cf03ab3dbc4dc05675c1c4044d63c02c00b07
nullbitcoder <lxxxl1146@gmail.com>
2021-01-23 17:21:35 +0100

    code set up for project 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render() {
     return (
-        <div className="App">
-            <h1>Hello From React!</h1>
-        </div>
+      <div className='App'>
+        <h1>Hello From React!</h1>
+      </div>
     );
+  }
 }
 
 export default App;
