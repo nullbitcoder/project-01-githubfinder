3a2879baf7eb441f56e855857ca226f49b1fd98a
nullbitcoder <lxxxl1146@gmail.com>
2021-01-23 18:01:42 +0100

    fragments, and variables with JSX

--- src/App.js
+++ src/App.js
@@ -16,3 +16,6 @@ export default App;
 
 // '"class"=Apop' in JSX is className
 // '<lab"for"='name'>Name</label> in JSX is htmlFor='name'
+
+//use "Fragments to replace <div className='App'></div>
+//can also use <> </> , but this can lead to some problems. Dont use.
