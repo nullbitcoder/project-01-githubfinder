e14467e405b92c21a7f74fe9be963d574c68a3ca
nullbitcoder <lxxxl1146@gmail.com>
2021-01-23 18:04:24 +0100

    using JavaScript mehtod within JSX

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ class App extends Component {
   render() {
     const name = 'John Doe'; //creating a variable
     return (
-      <Fragment>
-        <h1>Hello {name}</h1> {/* using the variable*/}
-      </Fragment>
+      <div className='App'>
+        <h1>Hello {name.toUpperCase()}</h1> {/* using JavaScript with JSX*/}
+      </div>
     );
   }
 }
