136768cb851982b5fd3e47251aa30e80bd1ca2e2
nullbitcoder <lxxxl1146@gmail.com>
2021-01-23 17:58:12 +0100

    using fragment, and variables

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
   render() {
+    const name = 'John Doe'; //creating a variable
     return (
-      <div className='App'>
-        <h1>Hello From React!</h1>
-      </div>
+      <Fragment>
+        <h1>Hello {name}</h1> {/* using the variable*/}
+      </Fragment>
     );
   }
 }
 
 export default App;
+
+// '"class"=Apop' in JSX is className
+// '<lab"for"='name'>Name</label> in JSX is htmlFor='name'
