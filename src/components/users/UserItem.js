ea4afae8d47fc14a412afdf1e2828266a6678daa
nullbitcoder <lxxxl1146@gmail.com>
2021-01-26 06:38:21 +0100

    refactor UserItem to use destructuring in prop arg

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const UserItem = (props) => {
+// destructuring in component args instead of in funct body
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   // destructuring props to use data
-  const { login, avatar_url, html_url } = props.user;
+  // const { login, avatar_url, html_url } = props.user;
 
   return (
     <div className='card text-center'>
