47b0db2ebc8320dd7f0e5ffc5fbad7248b3ffb70
nullbitcoder <lxxxl1146@gmail.com>
2021-01-26 06:27:41 +0100

    refactor UserItem as function component, no state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class UserItem extends Component {
-  render() {
-    // destructuring props to use data
-    const { login, avatar_url, html_url } = this.props.user;
+const UserItem = () => {
+  // destructuring props to use data
+  const { login, avatar_url, html_url } = this.props.user;
 
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
 
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-            More
-          </a>
-        </div>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserItem;
