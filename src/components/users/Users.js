20dc80213f0f722f02f6e09b2bcf64666c4bf38d
nullbitcoder <lxxxl1146@gmail.com>
2021-01-26 21:32:20 +0100

    API request for users, pass data in prop to Users

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,34 +2,35 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        id: '2',
-        login: 'defunkt',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt',
-      },
-      {
-        id: '3',
-        login: 'pjhyett',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett',
-      },
-    ],
-  };
+  // DON'T NEED THIS STATE, USERS ARE COMING IN AS PROPS.
+  // state = {
+  //   users: [
+  //     {
+  //       id: '1',
+  //       login: 'mojombo',
+  //       avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+  //       html_url: 'https://github.com/mojombo',
+  //     },
+  //     {
+  //       id: '2',
+  //       login: 'defunkt',
+  //       avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
+  //       html_url: 'https://github.com/defunkt',
+  //     },
+  //     {
+  //       id: '3',
+  //       login: 'pjhyett',
+  //       avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
+  //       html_url: 'https://github.com/pjhyett',
+  //     },
+  //   ],
+  // };
 
   render() {
     return (
       <div style={userStyle}>
-        {/* pass array of users through map() to user */}
-        {this.state.users.map((user) => (
+        {/* pass array of users through map() to user.. change this.state to this.props */}
+        {this.props.users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
         {/* ^ using props in a component */}
@@ -38,6 +39,7 @@ class Users extends Component {
   }
 }
 
+// style object, local component
 const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
