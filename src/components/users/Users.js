1484185b1df5eff6d937c0cafadc9bafb6194872
nullbitcoder <lxxxl1146@gmail.com>
2021-01-27 18:18:35 +0100

    create loading Spinner

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,45 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-  // DON'T NEED THIS STATE, USERS ARE COMING IN AS PROPS.
-  // state = {
-  //   users: [
-  //     {
-  //       id: '1',
-  //       login: 'mojombo',
-  //       avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-  //       html_url: 'https://github.com/mojombo',
-  //     },
-  //     {
-  //       id: '2',
-  //       login: 'defunkt',
-  //       avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-  //       html_url: 'https://github.com/defunkt',
-  //     },
-  //     {
-  //       id: '3',
-  //       login: 'pjhyett',
-  //       avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-  //       html_url: 'https://github.com/pjhyett',
-  //     },
-  //   ],
-  // };
-
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {/* pass array of users through map() to user.. change this.state to this.props */}
-        {this.props.users.map((user) => (
+        {users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
-        {/* ^ using props in a component */}
       </div>
     );
   }
-}
+};
 
-// style object, local component
+Users.PropTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+
 const userStyle = {
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
